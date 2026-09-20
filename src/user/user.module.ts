411acd9d558593a87934fb24adc16a9eb35d30ec
Cabral <erick.cabral.dutraa@gmail.com>
2026-09-19 23:24:55 -0300

    injeção de dependencia entreas as clases repository

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service.js';
 import { UserController } from './user.controller.js';
+import { UserPrismaRepository } from './repository/prisma/user.prisma.repository.js';
+import { UserRepository } from './repository/user.repository.js';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService,{
+    provide: UserRepository,
+    useClass: UserPrismaRepository,
+  }],
 })
 export class UserModule {}
+ 
