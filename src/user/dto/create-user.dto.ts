4d4c526b95390cefa02348dddf06337291cfe83d
Cabral <erick.cabral.dutraa@gmail.com>
2026-09-19 23:35:48 -0300

    user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsDateString, IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  nome: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string
+
+  @IsNotEmpty()
+  @IsDateString()
+  dataNascimeto: string;
+}
