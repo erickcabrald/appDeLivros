0cb1f5393d49b02b732c497e0b32030a2bd2413c
Cabral <erick.cabral.dutraa@gmail.com>
2026-09-20 23:11:37 -0300

    metodos create e getAllUser

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
+import { UserRepository } from './repository/user.repository.js';
+import { ResponseUserDto } from './dto/response-user.dto.js';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    private readonly userRepository: UserRepository,
+  ){}
+
+  async create(createUserDto: CreateUserDto): Promise<void> {
+    try {
+      await this.userRepository.createUser(createUserDto);
+    } catch (error: unknown) {
+
+      if (
+          typeof error === 'object' && // verificando se error é um objeto
+          error !== null && // verificando se error nã é um objeto nulo
+          'code' in error  && // verificando se code esta no objeto error
+          error.code === 'P2002' // verificando se error.code é igual a P2002 -> condigo que prisma retorna em caso de dados unicos iguais
+      ) {
+        throw new ConflictException('E-mail já cadrastado em outra conta');
+      }
+
+      throw new InternalServerErrorException('Não foi possivel criar o usuario');
+    }
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
+  async getAllUser(): Promise<ResponseUserDto[]> {
+    
+    try {
+      const users = await this.userRepository.getAllUser();
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+      return users;
+    } catch (error) {
+      throw new InternalServerErrorException('Não foi possivel buscar usuarios');
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
