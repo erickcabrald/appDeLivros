49e0c10214fa3659ca4a1602beead485a10f05d4
Cabral <erick.cabral.dutraa@gmail.com>
2026-09-21 23:35:47 -0300

    criação da documentação swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -15,6 +16,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Minha Api')
+    .setDescription('Documentação da API')
+    .setVersion('1.0.0')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('docs', app, document);
 
 
   await app.listen(process.env.PORT ?? 3000);
