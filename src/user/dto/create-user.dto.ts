732fd98096c671e14876eacb8896ad627b45313c
Cabral <erick.cabral.dutraa@gmail.com>
2026-09-21 23:36:50 -0300

    adicionado a propriadades para documentar entradas e saidas

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,20 +1,37 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDateString, IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Carlos',
+    description: 'nome do usuario'
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(4)
   name: string;
 
+  @ApiProperty({
+    example: 'carlosdam@gmail.com',
+    description: 'e-mail do usuario'
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'jub83db3',
+    description: 'senha do usuario'
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(6)
   password: string
 
+  @ApiProperty({
+    example: '2000-10-28',
+    description: 'data de nascimento'
+  })
   @IsNotEmpty()
   @IsDateString()
   data_nascimento: string;
--- src/user/dto/response-user.dto.ts
+++ src/user/dto/response-user.dto.ts
@@ -1,11 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
 
 
 export class ResponseUserDto {
+  @ApiProperty({
+    example: 'fm3if439fhf4nuekjjdjdd',
+    description: 'id do usuario em formato uuid'
+  })
   id: string;
 
+  @ApiProperty({
+    example: 'Carlos',
+    description: 'nome do usuario'
+  })
   name: string
 
+   @ApiProperty({
+    example: 'carlosdam@gmail.com',
+    description: 'e-mail do usuario'
+  })
   email: string;
 
+
+    @ApiProperty({
+    example: '2000-10-28',
+    description: 'data de nascimento'
+  })
   data_nascimento: string;
 }
